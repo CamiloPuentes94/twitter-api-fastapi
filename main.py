91a50f38c818537abff35e113b6e2f90b5bbb088
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-02-21 13:08:32 -0500

    Se agrega modelos de User y se crean modelos para la visuaizacion de informacion por seguridad, de igual manera se importan librerias de python y pydantic para las validaciones de los modelos

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+#Python
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
 # Pydantic
 from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
 
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)  #Se crea user_id para que no se repita el usuario con la libreria UUID de python
+    email: EmailStr = Field(...) #con la funcion Field(...) se realiza autenticacion para que sea obligatorio
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8
+    )
 class User(BaseModel):
-    pass
+    user_id: UUID = Field(...)  #Se crea user_id para que no se repita el usuario con la libreria UUID de python
+    email: EmailStr = Field(...) #con la funcion Field(...) se realiza autenticacion para que sea obligatorio
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+        )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+        )
+    birth_date: Optional[date] = Field(default=None)
 
 class Tweet(BaseModel):
     pass
