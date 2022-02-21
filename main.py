c9aa6249eebabe530c28bac9b765e7340bbf8201
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-02-21 12:53:12 -0500

    Se realiza organizacion de archivos que se importan, se importa BaseModel de pydantic y se crean los modelos de User y Tweet para luego darles la estructuracion

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
 
+class User(BaseModel):
+    pass
+
+class Tweet(BaseModel):
+    pass
+
+
+@app.get(path= "/")
+def home():
+    return{"twitter API": "Working!"}
